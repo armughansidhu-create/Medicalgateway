85a8042aa85bde188da2cf53dab230b5403866d8
Armughan <armughan.sidhu@gmail.com>
2026-04-02 14:32:41 -0700

    fix env vars

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -6,10 +6,3 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
-```
-
-Press Ctrl+S. Then in Terminal:
-```
-git add .
-git commit -m "use direct supabase client"
-git push
