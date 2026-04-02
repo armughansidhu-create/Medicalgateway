de75ba4be050d42060b5cc5d51b50457a0ddcda4
Armughan <armughan.sidhu@gmail.com>
2026-04-02 15:05:56 -0700

    fix client syntax

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -6,10 +6,3 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
-```
-
-Press **Ctrl+S** then in Terminal:
-```
-git add .
-git commit -m "fix client"
-git push
