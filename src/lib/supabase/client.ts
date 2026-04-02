ca09664453033c25186d01c7a13e50b40b1bfc68
Armughan <armughan.sidhu@gmail.com>
2026-04-02 14:53:31 -0700

    fix client

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,8 +2,8 @@ import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 export function createClient() {
   return createSupabaseClient(
-    'https://fggiqliqlqdijzmfglav.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZ2lxbGlxbHFkaWp6bWZnbGF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NTI5MzMsImV4cCI6MjA1OTEyODkzM30.Ex8fX3C-0_12BFTMGrJxDa7H7EFQClJ6sfj4EPnokHI'
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
 ```
@@ -11,5 +11,5 @@ export function createClient() {
 Press **Ctrl+S** then in Terminal:
 ```
 git add .
-git commit -m "hardcode supabase keys for testing"
+git commit -m "fix client"
 git push
