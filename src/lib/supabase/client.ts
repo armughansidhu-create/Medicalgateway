c85856ae9d98387ca3cc68b20452cbb1c6158eb6
Armughan <armughan.sidhu@gmail.com>
2026-04-02 14:39:26 -0700

    hardcode keys test

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,14 @@ import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 export function createClient() {
   return createSupabaseClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    'https://fggiqliqlqdijzmfglav.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZ2lxbGlxbHFkaWp6bWZnbGF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NTI5MzMsImV4cCI6MjA1OTEyODkzM30.Ex8fX3C-0_12BFTMGrJxDa7H7EFQClJ6sfj4EPnokHI'
   )
 }
+```
+
+Press **Ctrl+S** then in Terminal:
+```
+git add .
+git commit -m "hardcode supabase keys for testing"
+git push
